261671cfad1aaf61886f13ab5c5b36c65944e9df
maayaedo <166531017+maayaedo@users.noreply.github.com>
2024-05-20 23:56:21 +0900

    Delete 観光サイト directory

--- 観光サイト/js/navigation-control.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 250) {
-            $("header").css('top','0');
-        } else {
-            $("header").css('top','-200px');
-        }
-    });
-});
